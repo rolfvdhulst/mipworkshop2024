af781455e978432e43ff13d3da514d91c466971f
rolfvdhulst <rolfvdhulst@gmail.com>
2023-11-14 16:23:39 +0100

    Output log files for better debugging

--- runBenchmark.sh
+++ runBenchmark.sh
@@ -7,7 +7,7 @@ input="$PWD/data/first_round.test"
 N=6
 while IFS= read -r line
 do
-  ("$2"/build/apps/runIntegratedSCIP "$instanceFolder/$line.mps.gz" "$PWD/data/"; )&
+  ("$2"/build/apps/runIntegratedSCIP "$instanceFolder/$line.mps.gz" "$PWD/data/" > "$PWD/data/integratedOutput/$line.log" ;)&
   if [[ $(jobs -r -p | wc -l) -ge $N ]]; then
     wait -n
   fi
